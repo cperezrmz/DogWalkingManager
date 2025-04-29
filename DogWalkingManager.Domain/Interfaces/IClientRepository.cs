897d4bedf665fd38214e1cc7546393063a1ce63a
Cesar Perez <cesar.pr@gmail.com>
2025-04-29 08:44:58 -0600

    feat(domain): add IClientRepository interface for client operations

---
+++ DogWalkingManager.Domain/Interfaces/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogWalkingManager.Domain.Entities;
+
+namespace DogWalkingManager.Domain.Interfaces
+{
+    /// <summary>
+    /// Defines operations for managing clients.
+    /// </summary>
+    public interface IClientRepository
+    {
+        void Add(Client client);
+        void Delete(Guid clientId);
+        Client GetById(Guid id);
+        IEnumerable<Client> GetAll();
+    }
+}
