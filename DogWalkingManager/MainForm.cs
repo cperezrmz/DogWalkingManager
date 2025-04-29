b840a1631d1f413b6cac4fa949ee04edb1555268
Cesar Perez <cesar.pr@gmail.com>
2025-04-29 12:37:43 -0600

    feat(ui): add MainForm denpendencies

--- DogWalkingManager/MainForm.cs
+++ DogWalkingManager/MainForm.cs
@@ -1,9 +1,26 @@
+using DogWalkingManager.Infrastructure.DataContext;
+using DogWalkingManager.Infrastructure.Repositories;
+
 namespace DogWalkingManager;
 
+/// <summary>
+/// Main form for managing clients, dogs, and walks.
+/// </summary>
 public partial class MainForm : Form
 {
+ 
+    private readonly InMemoryContext _context;
+    private readonly ClientRepository _clientRepository;
+    private readonly DogRepository _dogRepository;
+    private readonly WalkRepository _walkRepository;
+
     public MainForm()
     {
         InitializeComponent();
+
+        _context = new InMemoryContext();
+        _clientRepository = new ClientRepository(_context);
+        _dogRepository = new DogRepository(_context);
+        _walkRepository = new WalkRepository(_context);
     }
 }
