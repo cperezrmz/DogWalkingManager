b58cc5ff489c754237392726073d2beb52cbbb7b
Cesar Perez <cesar.pr@gmail.com>
2025-04-29 08:45:57 -0600

    feat(domain): add IDogRepository interface for dog operations

---
+++ DogWalkingManager.Domain/Interfaces/IDogRepository.cs
@@ -0,0 +1,15 @@
+﻿using DogWalkingManager.Domain.Entities;
+
+namespace DogWalkingManager.Domain.Interfaces
+{
+    /// <summary>
+    /// Defines operations for managing dogs.
+    /// </summary>
+    public interface IDogRepository
+    {
+        void Add(Dog dog);
+        void Delete(Guid dogId);
+        Dog GetById(Guid id);
+        IEnumerable<Dog> GetAll();
+    }
+}
