c128ec96cb45dce34acb004c85b30f2c1181ab24
Cesar Perez <cesar.pr@gmail.com>
2025-04-29 08:55:57 -0600

    feat(domain): add IWalkRepository interface for walk operations

---
+++ DogWalkingManager.Domain/Interfaces/IWalkRepository.cs
@@ -0,0 +1,13 @@
+﻿using DogWalkingManager.Domain.Entities;
+
+namespace DogWalkingManager.Domain.Interfaces
+{
+    /// <summary>
+    /// Defines operations for managing dog walks.
+    /// </summary>
+    public interface IWalkRepository
+    {
+        void Add(Walk walk);
+        IEnumerable<Walk> GetAll();
+    }
+}
