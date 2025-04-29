bb57d569d44481bf3e3ad667e65f8f7598ed45d0
Cesar Perez <cesar.pr@gmail.com>
2025-04-29 12:24:09 -0600

    feat(infrastructure): add InMemoryContext for in-memory storage of entities

--- DogWalkingManager.Infrastructure/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DogWalkingManager.Infrastructure;
-public class Class1
-{
-
-}
---
+++ DogWalkingManager.Infrastructure/DataContext/InMemoryContext.cs
@@ -0,0 +1,15 @@
+﻿using DogWalkingManager.Domain.Entities;
+using System.Collections.Generic;
+
+namespace DogWalkingManager.Infrastructure.DataContext
+{
+    /// <summary>
+    /// Provides an in-memory data context for storing clients, dogs, and walks.
+    /// </summary>
+    public class InMemoryContext
+    {
+        public List<Client> Clients { get; set; } = new List<Client>();
+        public List<Dog> Dogs { get; set; } = new List<Dog>();
+        public List<Walk> Walks { get; set; } = new List<Walk>();
+    }
+}
